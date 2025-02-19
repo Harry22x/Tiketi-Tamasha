7094b4c9b74725ad28c8ac39772dc6dfa244ca73
Harry222x <ha.wakhule@gmail.com>
2025-02-19 16:50:40 +0300

    s

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,6 +1,7 @@
 import LandingPage from "./Pages/LandingPage";
 import App from "./App";
 import AddTicket from "./Pages/AddTicket";
+import EventPage from "./Pages/EventPage";
 
 const routes = [
   {
@@ -8,7 +9,8 @@ const routes = [
     element: <App />,
     children: [
       { path: "/", element: <LandingPage /> },
-      { path: "add-ticket", element: <AddTicket /> },
+      { path: "/add-ticket", element: <AddTicket /> },
+    {path:"/events/:id", element: <EventPage/>}
       
     ],
   },
