4ad5f84571b195be4922f110f4a11d2ddb8e0dc8
Harry222x <ha.wakhule@gmail.com>
2025-02-19 11:54:14 +0300

    SCRUM-5:created seed data for database

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request,make_response,session
 from flask_restful import Resource
 
 # Local imports
@@ -16,6 +16,40 @@ from models import User, UserTicket, Event, EventTicket
 @app.route('/')
 def index():
     return '<h1>Project Server</h1>'
+
+class Events(Resource):
+    def get(self):
+        events = [{
+            "id":event.id,
+            "name": event.name,
+            "description": event.description,
+            "date": event.date,
+            "time": event.time,
+            "image": event.image,
+        } for event in Event.query.all()]
+        return make_response(events,200)
+
+class EventByID(Resource):
+    def get(self, id):
+        event = Event.query.get(id)
+        if event:
+            return make_response(event.to_dict(),200)
+        else:
+            return make_response({"error":"Event does not exist"},404)
+
+
+
+api.add_resource(Events,'/events')
+api.add_resource(EventByID,'/events/<int:id>')
+
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
