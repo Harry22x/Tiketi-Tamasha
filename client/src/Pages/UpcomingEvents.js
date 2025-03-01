0cbd4bec25aab04c4e604c6760d01884a1d52395
Harry222x <ha.wakhule@gmail.com>
2025-03-01 13:41:15 +0300

    fixed netlify redirect issue

--- client/src/Pages/UpcomingEvents.js
+++ client/src/Pages/UpcomingEvents.js
@@ -30,7 +30,7 @@ const UpcomingEvents = ({ events }) => {
           </div>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-        {events.slice(0, 7).map((event, index) => (
+        {events.slice(0, 6).map((event, index) => (
   <EventCard key={index} {...event} />
 ))}
         </div>
