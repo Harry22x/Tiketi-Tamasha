d610180a122c29bde0ebdf5cf63d3d10e5e49f76
Maina <smurkcarter855@gmail.com>
2025-03-04 10:25:17 +0300

    SCRUM:SCRUM-42

--- client/src/components/EventCard.js
+++ client/src/components/EventCard.js
@@ -1,16 +1,19 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
+const EventCard = ({ id, name, date, time, image }) => {
+  const formattedDate = new Date(date).toDateString(); 
 
-const EventCard = ({ id, name, date, time, image }) => (
-  
-  <Link to={`/events/${id}`}>
-<div className="bg-white p-3 shadow-md h-full flex flex-col justify-between w-[27rem] mx-auto">
-      <img src={image} alt={name} className="w-full rounded-md" />
-      <h3 className="text-lg font-semibold mt-2">{name}</h3>
-      <p className="text-xs text-gray-500 mt-1">{new Date(date).toString().split("").splice(0,15).join("")} - {time}</p>
-    </div>
-    
-  </Link>
-);
+  return (
+    <Link to={`/events/${id}`}>
+      <div className="bg-white p-3 shadow-md h-full flex flex-col justify-between w-[27rem] mx-auto">
+        <img src={image} alt={name} className="w-full rounded-md" />
+        <h3 className="text-lg font-semibold mt-2">{name}</h3>
+        <p className="text-xs text-gray-500 mt-1">{formattedDate} - {time}</p>
+      </div>
+    </Link>
+  );
+};
 
 export default EventCard;
+
